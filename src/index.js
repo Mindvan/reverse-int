cb28e6ffd03bac199e8cb6e20abf83c9e4468a5a
Ivan Eroshin <itseroshin@gmail.com>
2023-09-28 18:52:37 +0300

    feat: image-gallery start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const string = Math.abs(n).toString();
+  var newString = '';
+
+  for (const char in string) {
+      newString += string[string.length - 1 - char]
+  }
+
+  return newString;
 }
